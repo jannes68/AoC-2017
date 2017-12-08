0ec3f2c28282ce89c3ac378a1c0720b439534373
Jan Slott <jan.slott@lm.se>
2017-12-08 11:13:57 +0100

    Day8 works

--- Day8.java
+++ Day8.java
@@ -5,15 +5,20 @@ import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
-public class Dag8 {
+public class Day8 {
 
 	/*
 
 	--- Day 8: I Heard You Like Registers ---
 	
-	You receive a signal directly from the CPU. Because of your recent assistance with jump instructions, it would like you to compute the result of a series of unusual register instructions.
+	You receive a signal directly from the CPU. Because of your recent assistance with jump instructions, 
+	it would like you to compute the result of a series of unusual register instructions.
 	
-	Each instruction consists of several parts: the register to modify, whether to increase or decrease that register's value, the amount by which to increase or decrease it, and a condition. If the condition fails, skip the instruction without modifying the register. The registers all start at 0. The instructions look like this:
+	Each instruction consists of several parts: the register to modify, whether to increase or decrease that register's value, 
+	the amount by which to increase or decrease it, and a condition. If the condition fails, 
+	skip the instruction without modifying the register. 
+	The registers all start at 0. 
+	The instructions look like this:
 	
 	b inc 5 if a > 1
 	a inc 1 if b < 5
@@ -27,28 +32,33 @@ public class Dag8 {
 	c is increased by -20 (to -10) because c is equal to 10.
 	After this process, the largest value in any register is 1.
 	
-	You might also encounter <= (less than or equal to) or != (not equal to). However, the CPU doesn't have the bandwidth to tell you what all the registers are named, and leaves that to you to determine.
+	You might also encounter <= (less than or equal to) or != (not equal to). 
+	However, the CPU doesn't have the bandwidth to tell you what all the registers are named, 
+	and leaves that to you to determine.
 	
 	What is the largest value in any register after completing the instructions in your puzzle input?
+
+	--- Part Two ---
+	
+	To be safe, the CPU also needs to know the highest value held in any register during this process 
+	so that it can decide how much memory to allocate to these operations. For example, in the above instructions, 
+	the highest value ever held was 10 (in register c after the third instruction was evaluated).	
 	
 	 */
 
 
 	public static void main(String[] args) {
 
-		List<Instr> instructions = data.stream().map(r -> new Instr(r)).collect(Collectors.toList());
-
-		instructions.forEach(i -> i.exec());
-
+		data.stream().map(r -> new Instr(r)).forEach(i -> i.exec());
 		int max = regMap.values().stream().mapToInt(v -> v).max().getAsInt();
 
-		regMap.keySet().stream().forEach(k -> System.out.println(k + "->" + regMap.get(k)));
-
-		System.out.println(max);
+		System.out.println("Part 1 max: " + max);
+		System.out.println("Part 2 maxEver:" + maxEver);
 
 	}
 
 	static Map<String, Integer> regMap = new HashMap<>();
+	static int maxEver = 0;
 
 	static int getReg(String regName) {
 		if (!regMap.containsKey(regName)) {
@@ -58,6 +68,9 @@ public class Dag8 {
 	}
 
 	static void setReg(String regName, int val) {
+		if (val > maxEver) {
+			maxEver = val;
+		}
 		regMap.put(regName, val);
 	}
 
@@ -73,7 +86,6 @@ public class Dag8 {
 		void exec() {
 			if (condOp.eval(condReg, condVal)) {
 				instrType.exec(instrRegister, instrVal);
-				System.out.println(this.instrRegister + " " + this.instrType + " " + this.instrVal + " -> " + getReg(this.instrRegister));
 			}
 		}
 
@@ -86,13 +98,13 @@ public class Dag8 {
 			this.condReg = parts[i++];
 			this.condOp = CondOp.fromName(parts[i++]);
 			this.condVal = Integer.parseInt(parts[i++]);
-
 		}
 
 	}
 
 	enum InstrType {
-		INC("inc", (r, v) -> r + v), DEC("dec", (r, v) -> r - v);
+		INC("inc", (r, v) -> r + v), 
+		DEC("dec", (r, v) -> r - v);
 
 		String name;
 		BiFunction<Integer, Integer, Integer> func;
@@ -119,8 +131,8 @@ public class Dag8 {
 		LTE("<=", (r, v) -> r <= v), 
 		GT(">", (r, v) -> r > v), 
 		GTE(">=", (r, v) -> r >= v), 
-		EQ("==", (r, v) -> r == v), 
-		NEQ("!=", (r, v) -> r != v);
+		EQ("==", (r, v) -> r.equals(v)), 
+		NEQ("!=", (r, v) -> !r.equals(v));
 
 		String name;
 		BiFunction<Integer, Integer, Boolean> func;
@@ -143,16 +155,6 @@ public class Dag8 {
 	}
 
 	static List<String> data = Arrays.asList(
-//			"b inc 5 if a > 1",
-//			"a inc 1 if b < 5",
-//			"c dec -10 if a >= 1",
-//			"c inc -20 if c == 10",
-//			"c dec -21 if c != 9",
-//			"c dec -22 if c <= 11",
-//			"c dec -23 if c < 34",
-//			"c dec -24 if c > 11",
-//			"a inc 1 if c < 58"
-			
 			"j inc -19 if jhb >= 10",
 			"es inc -432 if gbu <= -5",
 			"es inc 278 if ib > -9",
